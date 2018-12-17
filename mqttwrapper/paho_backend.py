d01dce26ff5cbe7909281aba53273918c87ecd60
Dave Arter <github@davea.me>
2018-12-17 22:42:02 +0000

    Add logging to paho_backend

--- mqttwrapper/paho_backend.py
+++ mqttwrapper/paho_backend.py
@@ -1,15 +1,25 @@
 import os
+import logging
 import paho.mqtt.client as mqtt
 
+log = logging.getLogger("mqttwrapper.paho_backend")
+
 def on_connect(client, userdata, flags, rc):
+    log.debug("Connected")
     for topic in userdata['topics']:
         client.subscribe(topic)
+        log.debug("Subscribed to %s", topic)
 
 def on_message(client, userdata, msg):
+    log.debug("Received message on %s", msg.topic)
     replies = userdata['callback'](msg.topic, msg.payload, **userdata['kwargs'])
+    log.debug("Callback completed.")
     if replies is not None:
+        log.debug("Received %s replies", len(replies))
         for topic, payload in replies:
+            log.debug("Publishing '%s' to %s", payload, topic)
             client.publish(topic, payload)
+            log.debug("Published '%s' to %s", payload, topic)
 
 
 def run_script(callback, broker=None, topics=None, **kwargs):
